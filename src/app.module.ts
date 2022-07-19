3ac5738f5388dfff16269ef4edf8fac2be11a4de
joannabiernat <j.biernat955@gmail.com>
2022-07-19 20:58:55 +0200

    change database name to head_hunters

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import {TypeOrmModule} from "@nestjs/typeorm";
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'head-hunter',
+      database: 'head_hunters',
       entities: [],
       synchronize: true,
     })
