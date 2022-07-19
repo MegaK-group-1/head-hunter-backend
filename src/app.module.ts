275e615137f5904a7be0a7b1bd414d94051241df
zaneta <zannien94@gmail.com>
2022-07-19 22:58:03 +0200

    add users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
+ import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
+  imports: [UsersModule, 
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'localhost', 
       port: 3306,
       username: 'root',
       password: 'root',
