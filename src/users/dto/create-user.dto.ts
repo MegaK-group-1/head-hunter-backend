a1ca6568eb5127ad6ddb3edf8e21cdfd6c09990e
Zaneta <zannien94@gmail.com>
2022-07-21 10:37:16 +0200

    validation for the user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,72 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  tel: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  githubUsername: string;
+
+  @IsString()
+  @IsOptional()
+  portfolioUrls: string;
+
+  @IsString()
+  @IsNotEmpty()
+  projectUrls: string;
+
+  @IsString()
+  @IsOptional()
+  bio: string;
+
+  @IsString()
+  @IsNotEmpty()
+  expectedTypeWork: string;
+
+  @IsString()
+  @IsOptional()
+  targetWorkCity: string;
+
+  @IsString()
+  @IsNotEmpty()
+  expectedContractType: string;
+
+  @IsString({})
+  @IsOptional()
+  expectedSalary: string;
+
+  @IsString()
+  @IsNotEmpty()
+  canTakeApprenticeship: string;
+
+  @IsString()
+  @IsNotEmpty()
+  monthsOfCommercialExp: string;
+
+  @IsString()
+  @IsOptional()
+  education: string;
+
+  @IsString()
+  @IsOptional()
+  workExperience: string;
+
+  @IsString()
+  @IsOptional()
+  courses: string;
+}
