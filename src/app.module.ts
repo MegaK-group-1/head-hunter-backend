2e2a7213742e8a95944bed7e905efd3561382a46
oohajo <oohajo@gmail.com>
2022-07-21 10:19:19 +0200

    database configuration update; connection to database established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import entities from "./typeorm";
+import entities, {Users} from "./typeorm";
+import {UserModule} from './typeorm/user.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'head-hunter',
-      entities,
-      synchronize: true,
-    })
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot(
+            {
+                type: 'mysql',
+                host: 'localhost',
+                port: 3306,
+                username: 'root',
+                password: '',
+                database: 'head_hunter',
+                entities,
+                bigNumberStrings: false,
+                logging: true,
+                synchronize: true,
+            }
+        ),
+        UserModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
