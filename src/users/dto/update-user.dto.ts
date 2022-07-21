4795785c02c1fbbcfea763b6fd3dee8721ae91ff
Zaneta <zannien94@gmail.com>
2022-07-22 00:30:45 +0200

    create user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  CreateUserDto,
+  CreateUserHrDto,
+  CreateUserAdminDto,
+} from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserHrDto extends PartialType(CreateUserHrDto) {}
+export class UpdateUserAdminDto extends PartialType(CreateUserAdminDto) {}
