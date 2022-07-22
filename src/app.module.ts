59a6e70b2100b1fe76630fdaaec11024eb60344d
Zaneta <zannien94@gmail.com>
2022-07-22 10:30:55 +0200

    test users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
- import { UsersModule } from './users/users.module';
-
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [UsersModule, 
+  imports: [
+    UsersModule,
+    AuthModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost', 
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'root',
       database: 'head_hunters',
       entities: [],
       synchronize: true,
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
