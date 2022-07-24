729beb5819392b62e2b6712c526830f1396106d6
Zaneta <zannien94@gmail.com>
2022-07-25 01:34:18 +0200

    check husky config

--- src/config/db/db.config.ts
+++ src/config/db/db.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 // TODO: usunac alternatywne wartosci, ma zostac tylko process.env
 export const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+   host: process.env.DB_HOST || 'localhost',
   port: 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD,
