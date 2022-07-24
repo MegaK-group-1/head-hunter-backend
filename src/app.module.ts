d437a70441409548cb409fee5e14cde733fe833e
Zaneta <zannien94@gmail.com>
2022-07-25 01:35:17 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,13 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { config } from './config/db/db.config';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {AuthModule} from './auth/auth.module';
+import {config} from './config/db/db.config';
 @Module({
-  imports: [
-    UsersModule,
-    AuthModule,
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(config),
-  ],
+  imports: [UsersModule, AuthModule, ConfigModule.forRoot(), TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
