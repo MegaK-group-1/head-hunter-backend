cef6cb65974bdb76ac8d2e7d9e9df078231af892
joannabiernat <j.biernat955@gmail.com>
2022-07-26 21:11:42 +0200

    installed and configured JWT strategy with cookie-parser package

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import {JwtStrategy} from "./jwt.strategy";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "../users/entities/user.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy]
 })
 export class AuthModule {}
